185f5da744ef7b5720826fc09f4cebd12a26bfc6
James Boullion <jboullion83@gmail.com>
2014-04-03 19:56:18 -0500

    Initial upload of rollaball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float jumpStrength;
+	public GUIText scoreText;
+	public GUIText victoryText;
+
+	private int score;
+
+	void Start (){
+		score = 0;
+		ClearText (victoryText);
+		PrintScore ();
+	}
+
+	//put physics here!!!!
+	void FixedUpdate (){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		float insideJump = 0.0f;
+		if (Input.GetKeyDown ("space")) {
+			insideJump = jumpStrength;
+		}
+
+		Vector3 movement = new Vector3(moveHorizontal, insideJump, moveVertical);
+		insideJump = 0.0f;
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive(false);
+			score++;
+			PrintScore ();
+			if(score >= 12){
+				victoryText.text = "Fuck Yes";
+			}
+		}
+	}
+
+	void PrintScore () {
+		scoreText.text = "Score: " + score.ToString();
+	}
+
+	void ClearText (GUIText guiText){
+		guiText.text = "";
+	}
+}
